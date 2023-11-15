64cce6c2336a0508ad22e017355752c3a7815437
Qcarvalhooliveira <124299955+Qcarvalhooliveira@users.noreply.github.com>
2023-11-15 22:59:30 +0100

    update for deploy

--- src/env/index.ts
+++ src/env/index.ts
@@ -11,8 +11,9 @@ if (process.env.NODE_ENV === 'test') {
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3333),
+  PORT: z.coerce.number().default(3333),
   JWT_KEY: z.string(),
   REFRESH_JWT_KEY: z.string(),
 })
